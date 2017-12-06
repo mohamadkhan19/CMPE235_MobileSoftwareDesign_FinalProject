26b520983e6fe59e3f3e51fdcbec944b65a19205
Mohamad Khan <mohamad@Mohamads-MBP.attlocal.net>
2017-12-06 01:08:34 -0800

    cart/add route added

--- models/cart.js
+++ models/cart.js
@@ -4,20 +4,17 @@ var Schema = mongoose.Schema;
 var User = require('./user');
 
 var schema = new Schema({
-    id: {type: Number},
-    photo: {type: String},
-    type: {type: String},
-    time: {type: Number},
-    header: {type: String},
-    status: {type: String},
-    text: {type: String},
-    comments: {type: Array},
+    service_id: {type: Number},
+    squarefeet: {type: Number},
+    duration: {type: String},
+    warranty: {type: String},
+    price: {type: Number},
     user_id: {type: Schema.Types.ObjectId, ref: 'User_id'}
 });
 
 schema.post('remove', function (form) {
     User.findById(form.user_id, function (err, user) {
-        user.cart_id.pull(form);
+        user.Cart_id.pull(form);
         user.save();
     });
 });
--- routes/cart.js
+++ routes/cart.js
@@ -28,14 +28,11 @@ router.post('/add', function (req, res, next) {
             });
         }
         var cart = new Cart({
-            id: req.body.id,
-            photo: req.body.photo,
-            type: req.body.type,
-            time: req.body.time,
-            header: req.body.header,
-            status: req.body.status,
-            text: req.body.text,
-            comments: req.body.comments,
+            service_id: req.body.service_id,
+            squarefeet: req.body.squarefeet,
+            duration: req.body.duration,
+            warranty: req.body.warranty,
+            price: req.body.price,
             user_id: user._id
         });
         cart.save(function (err, result) {
@@ -55,5 +52,24 @@ router.post('/add', function (req, res, next) {
         });
     });
 });
+
+// endpoint 'v1/cart/getservices'
+router.post('/getservices', function (req, res, next) {
+    Cart.find({"user_id":req.body.id})
+        .exec(function (err, result) {
+            if (err) {
+                return res.status(500).json({
+                    title: 'An error occurred',
+                    error: err
+                });
+            }
+            res.status(200).json({
+                message: 'Success',
+                success: 1,
+                obj: result
+            });
+        });
+});
+
 
 module.exports = router;
