286a2907aacdd6ab26a7123e55535c333f9f0428
Mohamad Khan <mohamad@Mohamads-MBP.attlocal.net>
2017-12-05 22:49:25 -0800

    service/getservices route added

--- routes/service.js
+++ routes/service.js
@@ -156,4 +156,22 @@ router.delete('/:id', function (req, res, next) {
     });
 });
 
+// endpoint 'v1/service/getservices'
+router.post('/getservices', function (req, res, next) {
+    Service.find({"user_id":req.body.id})
+        .exec(function (err, result) {
+            if (err) {
+                return res.status(500).json({
+                    title: 'An error occurred',
+                    error: err
+                });
+            }
+            res.status(200).json({
+                message: 'Success',
+                success: 1,
+                obj: result
+            });
+        });
+});
+
 module.exports = router;
