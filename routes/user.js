38b182356fe39acbf48909898b5f6e1646c2baae
Mohamad Khan <mohamad@Mohamads-MBP.attlocal.net>
2017-12-05 22:13:20 -0800

    user/vendors route added

--- routes/user.js
+++ routes/user.js
@@ -148,4 +148,22 @@ router.get('/customers', function (req, res, next) {
             });
         });
 });
+
+// endpoint 'v1/user/vendors'
+router.get('/vendors', function (req, res, next) {
+    User.find({type:"Vendor"})
+        .exec(function (err, result) {
+            if (err) {
+                return res.status(500).json({
+                    title: 'An error occurred',
+                    error: err
+                });
+            }
+            res.status(200).json({
+                message: 'Success',
+                success: 1,
+                obj: result
+            });
+        });
+});
 module.exports = router;
